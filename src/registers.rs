54c90c7edf46d58334369100fd8b567610d6401d
narwhaldisco <nward24@uwo.ca>
2020-06-07 11:34:06 -0400

    start registers

---
+++ src/registers.rs
@@ -0,0 +1,65 @@
+struct Registers {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: FlagsRegister,
+    h: u8,
+    l: u8,
+}
+
+const ZERO_FLAG_BYTE_POSITION: u8 = 7;
+const SUBTRACT_FLAG_BYTE_POSITION: u8 = 6;
+const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
+const CARRY_FLAG_BYTE_POSITION: u8 = 4;
+
+struct FlagsRegister {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool
+}
+
+// Allows us to convert a FlagRegister struct into a u8
+impl std::convert::From<FlagsRegister> for u8  {
+    fn from(flag: FlagsRegister) -> u8 {
+        (if flag.zero       { 1 } else { 0 }) << ZERO_FLAG_BYTE_POSITION |
+        (if flag.subtract   { 1 } else { 0 }) << SUBTRACT_FLAG_BYTE_POSITION |
+        (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_FLAG_BYTE_POSITION |
+        (if flag.carry      { 1 } else { 0 }) << CARRY_FLAG_BYTE_POSITION
+    }
+}
+
+// Allows us to convert the a u8 into a FlagRegister strcut
+impl std::convert::From<u8> for FlagsRegister {
+    fn from(byte: u8) -> Self {
+        let zero = ((byte >> ZERO_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let subtract = ((byte >> SUBTRACT_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let carry = ((byte >> CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+
+        FlagsRegister {
+            zero,
+            subtract,
+            half_carry,
+            carry
+        }
+    }
+}
+
+// Some registers functions
+impl Registers {
+    // gets the BC, 16 bit register
+    fn get_bc(&self) -> u16 {
+        (self.b as u16) << 8
+        | self.c as u16
+    }
+
+    // sets the BC, 16 big register
+    fn set_bc(&mut self, value: u16) {
+        self.b = ((value & 0xFF00) >> 8) as u8;
+        self.c = (value & 0xFF) as u8;
+    }
+} 
+  
